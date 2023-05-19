e44c2c6a191abf45bf39f7f3d26c34cfd93cef02
cebarrico <cbarrico@gmail.com>
2023-05-19 17:04:50 -0300

    test font

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { HomePage } from "./pages/home";
 import { AnimatedBackground } from "./components/Background";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./index.css";
 
 export const App = () => {
   const [loading, setLoading] = useState(true);
